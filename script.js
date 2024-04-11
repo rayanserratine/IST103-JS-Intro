36ae6914624be94ec68ba58cbcbf9ea61c8c4adc
rayanserratine <rayan.serratine@gmail.com>
2024-04-10 23:24:52 -0700

    console

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ let age = 26
 let boss = true
 let weird = ''
 
+console.log('The person is ' + personName)
+console.log('His age is ' + age)
+console.log('He is my boss? ' + boss)
+console.log(weird)
 
